29e1ecabebc4b5f9f37f77013ff64eb3f474fc58
«ArtemPetrov» <artempetrov112@gmail.com>
2022-09-15 16:25:36 +0400

    initial commit

---
+++ array.js
@@ -0,0 +1,106 @@
+const langArr = {
+    ".lang-key__title" :  {
+        "RU": "Портфолио",
+        "ENG": "Portfolio",
+    }, 
+    ".lang-key__h1": {
+        "RU": "Денис<br>Новик",
+        "ENG": "Denis<br>Novik",
+    },
+    ".lang-key__adaptiv-h1": {
+        "RU": "Денис<br>Новик",
+        "ENG": "Denis<br>Novik",
+    },  
+    ".lang-key__nav-link_home": {
+        "RU": "Главная",
+        "ENG": "Home",
+    }, 
+    ".lang-key__nav-link_about-me": {
+        "RU": "Обо мне",
+        "ENG": "About me",
+    }, 
+    ".lang-key__nav-link_skills": {
+        "RU": "Навыки",
+        "ENG": "Skills",
+    }, 
+    ".lang-key__nav-link_portfolio": {
+        "RU": "Портфолио",
+        "ENG": "Portfolio",
+    }, 
+    ".lang-key__nav-link_contacts": {
+        "RU": "Контакты",
+        "ENG": "Contacts",
+    },
+    ".lang-key__header-about": {
+        "RU": "UX | UI дизайнер<br> 24 года, Минск",
+        "ENG": "UX | UI designer<br> 24 years old, Minsk",
+    }, 
+    ".lang-key__adaptiv-header-about": {
+        "RU": "UX | UI дизайнер<br> 24 года, Минск",
+        "ENG": "UX | UI designer<br> 24 years old, Minsk",
+    }, 
+    ".lang-key__nav-popup-link_home": {
+        "RU": "Главная",
+        "ENG": "Home",
+    }, 
+    ".lang-key__nav-popup-link_about-me": {
+        "RU": "Обо мне",
+        "ENG": "About me",
+    }, 
+    ".lang-key__nav-popup-link_skills": {
+        "RU": "Навыки",
+        "ENG": "Skills",
+    }, 
+    ".lang-key__nav-popup-link_portfolio": {
+        "RU": "Портфолио",
+        "ENG": "Portfolio",
+    }, 
+    ".lang-key__nav-popup-link_contacts": {
+        "RU": "Контакты",
+        "ENG": "Contacts",
+    },
+    ".lang-key__about-me-h2": {
+        "RU": "Обо мне",
+        "ENG": "About me",
+    }, 
+    ".lang-key__about-me-p1": {
+        "RU": "Привет, я Денис — UX/UI дизайнер из Минска.<br class=\"adaptive-br\"> Интересуюсь дизайном и всем, что с ним связано.",
+        "ENG": "Hi, I'm Denis – UX/UI designer from Minsk.<br class=\"adaptive-br\"> I'm interested in design and everything connected with it.",
+    }, 
+    ".lang-key__about-me-p2": {
+        "RU": "Я учусь на курсах \"Веб и мобильный дизайн интерфейсов\" в IT-Академии.",
+        "ENG": "I'm studying at courses \"Web and mobile design interfaces\" in IT-Academy.",
+    },  
+    ".lang-key__about-me-p3": {
+        "RU": "Готов реализовать отличные проекты с замечательными людьми.",
+        "ENG": "Ready to implement excellent projects with wonderful people.",
+    },
+    ".lang-key__skills-h2": {
+        "RU": "Навыки",
+        "ENG": "Skills",
+    },  
+    ".lang-key__skills-par": {
+        "RU": "Работаю в таких программах как",
+        "ENG": "I work in such programs as",
+    }, 
+    ".lang-key__portfolio-h2": {
+        "RU": "Портфолио",
+        "ENG": "Portfolio",
+    },
+    ".lang-key__footer-h2": {
+        "RU": "Контакты",
+        "ENG": "Contacts",
+    },
+    ".lang-key__footer-btn": {
+        "RU": "Отправить сообщение",
+        "ENG": "Send message",
+    },
+    ".lang-key__footer-p1": {
+        "RU": "Хотите узнать больше или просто поболтать?<br>Добро пожаловать!",
+        "ENG": "Want to know more or just chat?<br>You are welcome!",
+    },
+    ".lang-key__footer-p2": {
+        "RU": "Поставьте мне лайк в <br>LinkedIn, Instagram, Behance, Dribble",
+        "ENG": "Like me on <br>LinkedIn, Instagram, Behance, Dribble",
+    },
+}
